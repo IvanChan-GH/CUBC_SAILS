206e0d4839298363208a62459fffff28a3992a68
Kwok Ka Chun <fatfat@Kwokde-MacBook-Pro.local>
2020-03-22 22:13:46 +0800

    delete function

--- config/routes.js
+++ config/routes.js
@@ -126,6 +126,8 @@ module.exports.routes = {
   'GET /category/category': 'CategoryController.showCategory',
   'POST /category/category': 'CategoryController.showCategory',
 
+  //'POST /estate/delete/:id': 'EstateController.delete',
+  'DELETE /user/:id': 'UserController.delete',
 
 
 
